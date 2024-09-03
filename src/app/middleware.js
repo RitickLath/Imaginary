6e0508ae0c889085cafafdc2ab1435523a573d41
RitickLath <ritick943lath@gmail.com>
2024-09-04 02:17:43 +0530

    Next Setup & Clerk Setup

---
+++ src/app/middleware.js
@@ -0,0 +1,39 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in",
+  "/sign-up",
+  "/home",
+]);
+
+const isPublicAPIRoute = createRouteMatcher(["/api/videos"]);
+
+export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
+  const currentUrl = new URL(req.url);
+  const isHomePage = currentUrl.pathname === "/home";
+
+  // if logged in and trying to access login or signup part redirect to home
+  if (userId && isPublicRoute(req) && !isHomePage) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+
+  // not logged in
+  if (!userId) {
+    if (!isPublicAPIRoute(req) && isPublicAPIRoute(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+  }
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
