0e1a9f13e3f8136e7f468750d856dc8e2d989529
RitickLath <ritick943lath@gmail.com>
2024-09-12 03:18:53 +0530

    Start the middlewares commented part

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in", "/sign-up"]);
+const isPublicRoute = createRouteMatcher(["/sign-in", "/sign-up", "/"]);
 
 export default clerkMiddleware((auth, req) => {
   const { userId } = auth(); // Access userId from auth object
