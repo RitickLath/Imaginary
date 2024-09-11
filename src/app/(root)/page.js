ebfac976265383dbc90d2bf5ecd2fc790712beae
RitickLath <ritick943lath@gmail.com>
2024-09-12 03:05:24 +0530

    Home Page content update

--- src/app/(root)/page.js
+++ src/app/(root)/page.js
@@ -1,23 +1,49 @@
-import { FaHome, FaCrop, FaMagic, FaRuler, FaImage } from "react-icons/fa";
+import {
+  FaHome,
+  FaCrop,
+  FaMagic,
+  FaRuler,
+  FaImage,
+  FaWrench,
+  FaFillDrip,
+  FaEdit,
+} from "react-icons/fa";
 import VideoCard from "../component/VideoCard";
 import { videoCardsData } from "../constants/VideoImage";
+import Link from "next/link";
 
 const facilities = [
+  { name: "Home", link: "/", icon: <FaHome className="w-4 h-4" /> },
   {
-    name: "Corners",
-    icon: <FaCrop className="  w-4 h-4" />,
+    name: "Round Corner",
+    link: "/round-corner",
+    icon: <FaCrop className="w-4 h-4" />,
   },
   {
-    name: "Image Enhancer",
-    icon: <FaMagic className="  w-4 h-4" />,
+    name: "AI Image Enhancer",
+    link: "/ai-image-enhancer",
+    icon: <FaMagic className="w-4 h-4" />,
   },
-  {
-    name: "Sharpen",
-    icon: <FaRuler className="  w-4 h-4" />,
-  },
+  { name: "Sharpen", link: "/sharpen", icon: <FaRuler className="w-4 h-4" /> },
   {
     name: "Background Removal",
-    icon: <FaImage className="   w-4 h-4" />,
+    link: "/bg-removal",
+    icon: <FaImage className="w-4 h-4" />,
+  },
+  {
+    name: "Generative Restore",
+    link: "/generative-restore",
+    icon: <FaWrench className="w-4 h-4" />,
+  },
+  {
+    name: "Generative Fill",
+    link: "/generative-fill",
+    icon: <FaFillDrip className="w-4 h-4" />,
+  },
+  {
+    name: "Generative Replace",
+    link: "/generative-replace",
+    icon: <FaEdit className="w-4 h-4" />,
   },
 ];
 
@@ -25,21 +51,26 @@ export default function Home() {
   return (
     <div className="py-6 px-3 lg:px-8 w-full flex flex-col items-center">
       <div className="px-4 md:px-10 bg-gradient-to-r from-[var(--from)] to-[var(--to)]  rounded-lg shadow-md  w-full md:w-[80%] flex flex-col items-center">
-        <h1 className="text-xl lg:text-3xl lg:font-semibold text-center max-w-[400px]   mt-6 lg:mt-12 ">
-          Bring Your Creative Vision <br /> to Life with Imaginary
+        <h1 className="text-xl font-serif font-semibold lg:text-3xl lg:font-semibold text-center max-w-[500px]   mt-6 lg:mt-12 ">
+          IMAGINARY: REIMAGINE YOUR SHOTS
+          {/* Bring Your Creative Vision <br /> to Life with Imaginary */}
         </h1>
 
         {/* AI Facilities Section */}
         <div className="mt-6 mb-6 lg:mb-12 lg:mt-12 grid grid-cols-4 gap-2 sm:gap-4">
           {facilities.map((facility, index) => (
-            <div key={index} className="flex flex-col items-center">
+            <Link
+              href={facility.link || ""}
+              key={index}
+              className="flex flex-col items-center"
+            >
               <div className="shadow-xl p-3 rounded-full mb-2 flex items-center justify-center">
                 {facility.icon}
               </div>
               <span className="  text-center text-xs sm:text-sm md:text-base">
                 {facility.name}
               </span>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
