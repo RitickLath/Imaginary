35304c5dbc31121ef147543c8fea0a7c9f33a0fd
RitickLath <ritick943lath@gmail.com>
2024-09-12 18:01:54 +0530

    sidebar z index

--- src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="z-0 w-full max-w-[200px] pt-6 pb-3 mx-auto flex justify-center">
+    <div className="z-0 w-full max-w-[200px] sm:pt-6 pb-3 mx-auto flex justify-center">
       <div className="">
         <SignIn />
       </div>
