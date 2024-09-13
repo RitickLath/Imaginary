5480a80ab8c2f5b8ba0bdfa60377cf8450875312
RitickLath <ritick943lath@gmail.com>
2024-09-14 00:51:35 +0530

    check

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in", "/sign-up", "/"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in",
+  "/sign-up",
+  "/",
+  "/ai-image-enhancer",
+  "/bg-removal",
+  "/generative-fill",
+  "/generative-restore",
+  "/generative-replace",
+  "/round-corner",
+  "/sharpen",
+]);
 
 export default clerkMiddleware((auth, req) => {
   const { userId } = auth(); // Access userId from auth object
@@ -9,17 +20,11 @@ export default clerkMiddleware((auth, req) => {
 
   // If the user is logged in and accessing a public route, redirect to the home page
   if (userId && isPublicRoute(req)) {
-    // console.log(
-    //   "User is logged in and trying to access a public route, redirecting to /"
-    // );
     return NextResponse.redirect(new URL("/home", currentUrl));
   }
 
   // If the user is not logged in and accessing a protected route, redirect to the sign-up page
   if (!userId && !isPublicRoute(req)) {
-    // console.log(
-    //   "User is not logged in and trying to access a protected route, redirecting to /sign-up"
-    // );
     return NextResponse.redirect(new URL("/sign-up", currentUrl));
   }
 
