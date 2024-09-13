bb338c1d2d58b34c58ac7a8fa0ed62925e143bad
RitickLath <ritick943lath@gmail.com>
2024-09-14 02:31:40 +0530

    prisma

--- src/app/prisma.js
+++ src/app/prisma.js
@@ -1,13 +1,9 @@
 // prisma.ts or prisma.js
 import { PrismaClient } from "@prisma/client";
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+const prisma =
+  process.env.NODE_ENV === "production"
+    ? new PrismaClient()
+    : global.prisma || (global.prisma = new PrismaClient());
 
 export default prisma;
